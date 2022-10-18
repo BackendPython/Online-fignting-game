7a4947173445174ea833de2ea3c2edfd377c096b
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-18 11:58:32 +0500

    add profile class

--- battle/models.py
+++ battle/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomeUser(AbstractUser):
     pass
+
+class UserProfile(models.Model):
+    img = models.ImageField(default='static/images/user.png')
