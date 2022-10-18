39a778d6bc91c9c20b1210947974e67066969fb1
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-18 13:17:46 +0500

    user image

--- battle/models.py
+++ battle/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomeUser(AbstractUser):
-    image = models.ImageField(default='static/images/user.png')
+    image = models.ImageField()
 
 
