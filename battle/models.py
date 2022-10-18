adcd33a697932a01f3ea05519f4673ac7afc5c5d
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-18 13:39:02 +0500

    add login logout

--- battle/models.py
+++ battle/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomeUser(AbstractUser):
     image = models.ImageField()
     age = models.IntegerField(default=20)
+    all_fighting = models.IntegerField(default=0)
     region = models.CharField(default="Uzbekistan", max_length=50)
     bio = models.CharField(default="I'm best fighter in the world", max_length=200)
 
