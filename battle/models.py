f314f3438ae3d3c4dae7e332a466cc403d724312
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-18 14:21:19 +0500

    img error fixed

--- battle/models.py
+++ battle/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomeUser(AbstractUser):
     image = models.ImageField()
     age = models.IntegerField(default=20)
+    victory = models.IntegerField(default=0)
+    defeats = models.IntegerField(default=0)
     all_fighting = models.IntegerField(default=0)
     region = models.CharField(default="Uzbekistan", max_length=50)
     bio = models.CharField(default="I'm best fighter in the world", max_length=200)
