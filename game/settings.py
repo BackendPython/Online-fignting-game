4c75cad1ba1f8d4fb1e3ceecb6c33fce594a465c
backendpython <holmirzaevsohruh196@gmail.com>
2022-10-18 11:14:49 +0500

    AbstractUser

--- game/settings.py
+++ game/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'game',
+    'battle',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +139,4 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'battle.CustomeUser'
